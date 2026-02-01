9c52413cfde1f09cd906d0324e99eaf50d267ae2
Lena <apasihsh@gmail.com>
2026-02-01 13:44:47 +0700

    vc

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import app from '../dist/index.cjs';
-
-// In a Vercel serverless function with type: module, importing a CJS file 
-// (which esbuild bundled into module.exports or exports.default) 
-// might result in the default export being wrapped.
-// We export the app directly.
-export default app;
